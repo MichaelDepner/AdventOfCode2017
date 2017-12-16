195e939e85b822fe27429f428f7da045b7c086e8
Michael Frikke Madsen <mibias.madsen@gmail.com>
2017-12-16 23:40:09 +0100

    Day 3 Part 1 implementation

---
+++ Day3/Day3_1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Utils;
+
+namespace Day3
+{
+    public class Day3_1 : ISolver
+    {
+        public string PuzzleName => "Day 3 Part 1";
+
+        public string Solve(string input)
+        {
+            int goal = Convert.ToInt32(input);
+            var coords = FindCoordsIterative(goal);
+            var manhattanDistance = Math.Abs(coords.Item1) + Math.Abs(coords.Item2);
+            return manhattanDistance.ToString();
+        }
+
+        private Tuple<int, int> FindCoordsIterative(int goal)
+        {
+            int x = 0;
+            int y = 0;
+            int currentNumber = 1;
+            int directionSwitch = 1;
+            int currentDistance = 0;
+            int maxDistance = 1;
+
+            while ( currentNumber != goal )
+            {
+                if (currentDistance == maxDistance)
+                {
+                    switch (directionSwitch)
+                    {
+                        case 1:
+                            currentDistance = 0;
+                            directionSwitch++;
+                            break;
+                        case 2:
+                            currentDistance = 0;
+                            directionSwitch++;
+                            maxDistance++;
+                            break;
+                        case 3:
+                            currentDistance = 0;
+                            directionSwitch++;
+                            break;
+                        case 4:
+                            currentDistance = 0;
+                            directionSwitch = 1;
+                            maxDistance++;
+                            break;
+                    }
+                }
+                else
+                {
+                    currentNumber++;
+                    currentDistance++;
+                    switch (directionSwitch)
+                    {
+                        case 1:
+                            x++;
+                            break;
+                        case 2:
+                            y++;
+                            break;
+                        case 3:
+                            x--;
+                            break;
+                        case 4:
+                            y--;
+                            break;
+                    }
+                }
+            }
+            return new Tuple<int, int>(x, y);
+        }
+    }
+}
---
+++ Day3/Day3_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils;
+
+namespace Day3
+{
+    class Day3_2 : ISolver
+    {
+        public string PuzzleName => "Day 3 Part 2";
+
+        public string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
